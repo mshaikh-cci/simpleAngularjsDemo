0a007348464de3c98d851b3eeffc5e470d5a579d
ccimubina <mubina.shaikh@creativecapsule.com>
2019-04-26 18:36:33 +0530

    other things of angularjs learnt

---
+++ test2/scripts/JavaScript.js
@@ -0,0 +1,13 @@
+﻿var myapp = angular.module("myapp", [])
+
+myapp.controller("HelloController", function ($scope) {
+    var employee = {
+        firstname: "david",
+        lastname: "shaikh",
+        gender: "male"
+    }
+    $scope.employee = employee;
+        $scope.helloTo = {};
+        $scope.helloTo.title = "AngularJS";
+        $scope.message = "Angularjs tutorial";
+    });
